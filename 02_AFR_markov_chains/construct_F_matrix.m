51701b1faea497b7eb15b4322175c4a773dee19d
anonymous-author-JAE-2025 <etienne.rouby@colorado.edu>
2025-08-12 09:31:01 -0600

    Delete 02_AFR_markov_chains directory

--- 02_AFR_markov_chains/construct_F_matrix.m
+++
@@ -1,4 +0,0 @@
-% construct_F_matrix.m
-function F = construct_F_matrix()
-    F = zeros(18, 18);  % 18x18 matrix of zeros
-end
--- 02_AFR_markov_chains/logit.m
+++
@@ -1,6 +0,0 @@
-function [ x ] = logit(x)
-x(x==0) = x(x==0)+1e-10;
-x(x==1) = x(x==1)-1e-10;
-x = log(x./(1-x));
-end
-
